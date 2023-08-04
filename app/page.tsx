6906e01c3a5f1884ab2f45bedcb2722043c7b364
johnny2smooth <johnbp13@gmail.com>
2023-08-03 19:49:40 -0700

    import optimized font from next

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home() {
   return (
@@ -8,6 +11,7 @@ export default function Home() {
       <div className="ff-cursive text-6xl p-4">Cursive</div>
       <div className="ff-arial text-6xl p-4">Arial</div>
       <div className="ff-eb-garamond text-6xl p-4">Arial</div>
+      <div className={`${inter.className} text-6xl p-4`}>Inter from Next!</div>
     </>
   );
 }
